b6b79e24e6ddefc871b9b3a388d40a274801073a
adelineberte <adeline.berte@hetic.net>
2019-01-26 16:21:54 +0100

    Index 🤙🏻

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	// include 'database/config.php';
+	include 'includes/header/head.php';
+	include 'includes/header/nav.php';
+	include 'includes/home/1-section.php';
+	include 'includes/home/2-section.php';
+	include 'includes/home/3-section.php';
+	include 'includes/home/4-section.php';
+	include 'includes/home/6-section.php';
+	include 'includes/home/7-section.php';
+?>
+
+<!-- SCRITPS -->
+<?php include 'includes/scripts.php';?>	
+
